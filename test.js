8a66d157d2603b7059e04ecea8e31472a3e46512
think1 <think1@localhost.localdomain>
2026-02-17 17:22:05 -0500

    good

---
+++ test.js
@@ -0,0 +1,47 @@
+/*
+Copyright 2016 Certified CoderZ
+Author: Brandon Poole Sr. (biz9framework@gmail.com)
+License GNU General Public License v3.0
+Description: BiZ9 Framework: Data - Test
+*/
+const async = require('async');
+const assert = require('node:assert');
+const {Log} = require("biz9-utility");
+const {Type} = require("biz9-logic");
+/*
+ * availble tests
+- connect
+*/
+/* --- TEST CONFIG START --- */
+const APP_ID = 'test-stage-feb17';
+/* --- TEST CONFIG END --- */
+
+/* --- DATA CONFIG END --- */
+//9_connect - 9_test_connect
+describe('connect', function(){ this.timeout(25000);
+    it("_connect", function(done){
+        let error=null;
+        let database = {};
+        let data = {};
+        async.series([
+            async function(call){
+                //-->
+                let print_test = true;
+                //-->
+                //-- BLANK START --//
+                //let parent = Data_Logic.get(Type.DATA_PRODUCT,'929');
+                //const [error,biz_data] = await Portal.get(database,parent.data_type,parent.id,option);
+                //-- BLANK END --//
+                //---
+                if(print_test){;
+                    Log.w('99_biz_data',biz_data);
+                }
+            },
+        ],
+            function(error, result){
+                console.log('CONNECT-DONE');
+                done();
+            });
+    });
+});
+
